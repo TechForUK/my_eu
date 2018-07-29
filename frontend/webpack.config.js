f8dee5448d48937080b7dc5fdc6f9c71bab7e64b
John Lees-Miller <jdleesmiller@gmail.com>
2018-07-29 11:00:47 +0100

    Set up frontend tooling

---
+++ frontend/webpack.config.js
@@ -0,0 +1,30 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+module.exports = {
+  mode: process.env.NODE_ENV || 'development',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: { minimize: true }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html'
+    })
+  ]
+}
