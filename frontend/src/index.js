3c17f4ebf7d12b2d27ac3df65bbbbfe9c2993cff
John Lees-Miller <jdleesmiller@gmail.com>
2018-07-29 11:20:59 +0100

    Add bootstrap for styling

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,1 +1,4 @@
+import 'bootstrap'
+import './scss/index.scss'
+
 import App from './app'
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -11,6 +11,32 @@ module.exports = {
         }
       },
       {
+        test: /\.scss$/,
+        use: [
+          {
+            // Adds CSS to the DOM by injecting a `<style>` tag
+            loader: 'style-loader'
+          },
+          {
+            // Interprets `@import` and `url()` like `import/require()` and will resolve them
+            loader: 'css-loader'
+          },
+          {
+            // Loader for webpack to process CSS with PostCSS
+            loader: 'postcss-loader',
+            options: {
+              plugins: function() {
+                return [require('autoprefixer')]
+              }
+            }
+          },
+          {
+            // Loads a SASS/SCSS file and compiles it to CSS
+            loader: 'sass-loader'
+          }
+        ]
+      },
+      {
         test: /\.html$/,
         use: [
           {
