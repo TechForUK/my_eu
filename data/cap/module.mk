37a4a7cdd6a2e93f29e388173c2830b975d830bd
John Lees-Miller <jdleesmiller@gmail.com>
2018-08-12 15:49:04 +0100

    Save CAP by area data

--- data/cap/module.mk
+++ data/cap/module.mk
@@ -1,5 +1,7 @@
-CAP_OUTPUTS := cap/output/cap_by_area.geo.json
+CAP_OUTPUTS := \
+	cap/output/cap_by_area.geo.json \
+	cap/output/cap_by_area.pkl.gz
 OUTPUTS += $(CAP_OUTPUTS)
 
 cap/cap_clean.ipynb.html: cap/cap_clean.ipynb cap/input/**/* postcodes/input/**/* $(POSTCODES_OUTPUTS)
-cap/output/cap_by_area.geo.json: cap/cap_clean.ipynb.html
+$(CAP_OUTPUTS): cap/cap_clean.ipynb.html
