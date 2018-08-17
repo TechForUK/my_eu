a713ec521de346720c3d88923a79beeb538d8db3
John Lees-Miller <jdleesmiller@gmail.com>
2018-08-17 22:38:51 +0100

    Tidy up ESIF cleaning for England and Scotland

--- data/esif/module.mk
+++ data/esif/module.mk
@@ -1,6 +1,13 @@
-ESIF_OUTPUTS := esif/output/beneficiaries.geo.json \
-	esif/output/erdf_england_2014_2020.pkl.gz
-OUTPUTS += $(ESIF_OUTPUTS)
+ESIF_ENGLAND_OUTPUTS := esif/output/beneficiaries.geo.json \
+	esif/output/esif_england_2014_2020.pkl.gz
+OUTPUTS += $(ESIF_ENGLAND_OUTPUTS)
 
-esif/esif_clean.ipynb.html: esif/esif_clean.ipynb esif/input/**/* postcodes/input/**/* $(POSTCODES_OUTPUTS)
-$(ESIF_OUTPUTS): esif/esif_clean.ipynb.html
+esif/england_esif_clean.ipynb.html: esif/england_esif_clean.ipynb esif/input/**/* postcodes/input/**/*
+$(ESIF_ENGLAND_OUTPUTS): esif/england_esif_clean.ipynb.html
+
+ESIF_SCOTLAND_OUTPUTS := esif/output/scotland_data.geo.json \
+	esif/output/esif_scotland.pkl.gz
+OUTPUTS += $(ESIF_SCOTLAND_OUTPUTS)
+
+esif/scotland_esif_clean.ipynb.html: esif/scotland_esif_clean.ipynb esif/input/**/* postcodes/input/**/*
+$(ESIF_SCOTLAND_OUTPUTS): esif/scotland_esif_clean.ipynb.html
