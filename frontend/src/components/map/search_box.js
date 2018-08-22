e1c92af68f7691e5948b1f5bec6e34055d80c88b
John Lees-Miller <jdleesmiller@gmail.com>
2018-08-22 23:08:12 +0100

    Handle enter key in search box

--- frontend/src/components/map/search_box.js
+++ frontend/src/components/map/search_box.js
@@ -2,20 +2,21 @@
 
 export default function setUpSearchBox(googleMaps, map) {
   const input = document.getElementById('my-eu-search-box')
-  const autocomplete = new googleMaps.places.Autocomplete(input, {
+  const autocompleteOptions = {
     types: ['geocode'],
     componentRestrictions: { country: ['UK'] }
-  })
+  }
+  const autocomplete = new googleMaps.places.Autocomplete(
+    input,
+    autocompleteOptions
+  )
 
   // Only return fields we need, to avoid paying lots of money.
-  autocomplete.setFields(['geometry', 'formatted_address', 'icon'])
+  const autocompleteFields = ['geometry', 'formatted_address', 'icon']
+  autocomplete.setFields(autocompleteFields)
 
   // Bias the results toward the current view.
   autocomplete.bindTo('bounds', map)
-
-  // TODO better hacks: https://stackoverflow.com/a/28100636/2053820
-  // but might still want the below to prevent a form submit... but then again
-  // maybe just block the form submit more directly.
 
   // Don't submit the form if the user presses enter.
   googleMaps.event.addDomListener(input, 'keydown', function(event) {
@@ -27,11 +28,14 @@ export default function setUpSearchBox(googleMaps, map) {
   let marker = null
   autocomplete.addListener('place_changed', function() {
     const place = autocomplete.getPlace()
-    if (!place.geometry) {
-      alert("Sorry, we couldn't find that place. Please try again.")
-      return
+    if (place.geometry) {
+      markPlace(place)
+    } else {
+      lookUpPlacePrediction(place)
     }
+  })
 
+  function markPlace(place) {
     // Clear out the old marker.
     if (marker) {
       marker.setMap(null)
@@ -68,5 +72,44 @@ export default function setUpSearchBox(googleMaps, map) {
     if (map.getZoom() > 12) {
       map.setZoom(12)
     }
-  })
+  }
+
+  // User pressed enter, which doesn't complete the address. Use the
+  // autocomplete service to try to find what they wanted.
+  // Based on https://stackoverflow.com/a/28100636/2053820
+  function lookUpPlacePrediction(place) {
+    const autocompleteService = new googleMaps.places.AutocompleteService()
+    const placePredictionOptions = Object.assign({}, autocompleteOptions, {
+      input: place.name,
+      offset: place.name.length
+    })
+    autocompleteService.getPlacePredictions(
+      placePredictionOptions,
+      function receivePlacePredictions(list, _status) {
+        if (list && list.length > 0) {
+          markPlacePrediction(list[0])
+        } else {
+          showNotFoundError()
+        }
+      }
+    )
+  }
+
+  function markPlacePrediction(prediction) {
+    const placesService = new googleMaps.places.PlacesService(map)
+    placesService.getDetails(
+      { fields: autocompleteFields, placeId: prediction.place_id },
+      function receivePlaceDetails(place, _status) {
+        if (place && place.geometry) {
+          markPlace(place)
+        } else {
+          showNotFoundError()
+        }
+      }
+    )
+  }
+
+  function showNotFoundError() {
+    alert("Sorry, we couldn't find that place. Please try again.")
+  }
 }
