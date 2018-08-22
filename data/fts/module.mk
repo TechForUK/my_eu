198b26abcb5b0425dbb04c49a54bbfdf3f8c29e3
John Lees-Miller <jdleesmiller@gmail.com>
2018-08-22 18:14:15 +0100

    Add currency conversion to GBP and FTS2017 data

--- data/fts/module.mk
+++ data/fts/module.mk
@@ -1,6 +1,11 @@
-FTS_2016_OUTPUTS := fts/output/fts2016_data.geo.json \
-	fts/output/fts_2016.pkl.gz
+FTS_2016_OUTPUTS := fts/output/fts_2016.pkl.gz
 OUTPUTS += $(FTS_2016_OUTPUTS)
 
 fts/fts_2016_clean.ipynb.html: fts/fts_2016_clean.ipynb fts/input/**/* postcodes/input/**/*
 $(FTS_2016_OUTPUTS): fts/fts_2016_clean.ipynb.html
+
+FTS_2017_OUTPUTS := fts/output/fts_2017.pkl.gz
+OUTPUTS += $(FTS_2017_OUTPUTS)
+
+fts/fts_2017_clean.ipynb.html: fts/fts_2017_clean.ipynb fts/input/**/* postcodes/input/**/*
+$(FTS_2017_OUTPUTS): fts/fts_2017_clean.ipynb.html
