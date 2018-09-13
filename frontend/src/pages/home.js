751706ec99dc6acd384c978738835d417225a7a0
John Lees-Miller <jdleesmiller@gmail.com>
2018-09-13 08:23:53 +0100

    Tidy up twitter card tags

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -4,16 +4,17 @@ import { Meta, Title } from 'react-head'
 import App from '../components/app'
 import { SITE_DESCRIPTION } from '../strings'
 
+const TITLE = 'myeu.uk – see what the EU has done for you'
+
 const Home = () => (
   <React.Fragment>
-    <Title>myeu.uk - see what the EU has done for you</Title>
+    <Title>{TITLE}</Title>
     <Meta property="og:type" content="website" />
     <Meta property="og:description" content={SITE_DESCRIPTION} />
     <Meta name="description" content={SITE_DESCRIPTION} />
-    <Meta name="description" content={SITE_DESCRIPTION} />
     <Meta name="twitter:card" content="summary_large_image" />
     <Meta name="twitter:site" content="@myeuuk" />
-    <Meta name="twitter:title" content="myeu.uk - see what the EU has done for you" />
+    <Meta name="twitter:title" content={TITLE} />
     <Meta name="twitter:description" content={SITE_DESCRIPTION} />
     <div id="my-eu-root">
       <App />
